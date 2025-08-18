b75c2a461c869722a8bace05f2fcd7beb94c58d9
gtortajada <gtortajada1@gmail.com>
2025-08-18 09:48:43 -0300

    feat: navbar v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import AbouthUs from "@/components/sections/aboutUs/aboutUs";
-import { FAQ } from "@/components/sections/faq/faq";
+import AboutUs from "@/components/sections/aboutUs/aboutUs";
+import FAQ from "@/components/sections/faq/faq";
 import Hero from "@/components/sections/hero/hero";
 import Services from "@/components/sections/services/services";
-import { Testimonials } from "@/components/sections/testimonials/testimonials";
+import Testimonials from "@/components/sections/testimonials/testimonials";
 
 export default function Page() {
   return (
     <>
       <Hero />
       <Services />
-      <AbouthUs />
+      <AboutUs />
       <Testimonials />
       <FAQ />
     </>
