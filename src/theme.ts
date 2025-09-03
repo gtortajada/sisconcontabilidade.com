0bd34d0d7b0859ff4f644c18b31528739f8e5fb1
Gustavo Almeida <gspalmeida@icloud.com>
2025-09-03 14:31:35 -0300

    refactor: added Arial as fallback font

--- src/theme.ts
+++ src/theme.ts
@@ -3,12 +3,12 @@
 import { createTheme } from '@mantine/core';
 
 export const theme = createTheme({
-  fontFamily: 'Aeonik, sans-serif',
+  fontFamily: "Aeonik, Arial, sans-serif",
   headings: {
-    fontFamily: 'Aeonik, sans-serif',
+    fontFamily: "Aeonik, Arial, sans-serif",
     sizes: {
-      h1: { fontSize: '3rem', fontWeight: '700' },
-      h2: { fontSize: '2.5rem', fontWeight: '300' },
+      h1: { fontSize: "3rem", fontWeight: "700" },
+      h2: { fontSize: "2.5rem", fontWeight: "300" },
     },
   },
 }); 
