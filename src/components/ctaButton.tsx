f42a55a2c5b30edde0c5f967a0907bfbe2022359
gtortajada <gtortajada1@gmail.com>
2025-09-03 09:47:34 -0300

    feat: Adição de mensagem e função encaminhar ao botão de contato.

--- src/components/ctaButton.tsx
+++ src/components/ctaButton.tsx
@@ -14,6 +14,7 @@ export function CtaButton({ children }: CtaButtonProps) {
       size="lg"
       fw={600}
       px="xl"
+      onClick={() => window.open("https://wa.me/554430266080?text=Olá,%20entrei%20em%20contato%20através%20do%20site%20e%20quero%20esclarecer%20algumas%20dúvidas.", "_blank")}
     >
       {children}
     </Button>
